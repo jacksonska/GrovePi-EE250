feab25d659257d30040f395e96a57b11823c0323
Jackson Skaaden EE250 VM : Windows Host <jskaaden@usc.edu>
2020-08-28 16:09:17 -0700

    Added more to test lcd

--- Software/Python/grove_rgb_lcd/example.py
+++ Software/Python/grove_rgb_lcd/example.py
@@ -35,14 +35,23 @@ THE SOFTWARE.
 
 from grove_rgb_lcd import *
 
+
 # setText("Hello world\nLCD test")
 setText("Yo")
 setRGB(0,128,64)
 
-# Slowly change the colors every 0.01 seconds.
-for c in range(0,255):
-    setRGB(c,255-c,0)
-    time.sleep(0.01)
+setRGB(255,0,0)
+setText("Red Light")
 
+time.sleep(100)
 setRGB(0,255,0)
+setRGB("Green Light")
+time.sleep(100)
+
+# Slowly change the colors every 0.01 seconds.
+# for c in range(0,255):
+#     setRGB(c,255-c,0)
+#     time.sleep(0.01)
+
+setRGB(0,0,255)
 setText("Bye bye, this should wrap")
